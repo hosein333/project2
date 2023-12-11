13d7a19714c6b7cff4e579ca29396bd955efd2fb
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-11 18:47:01 +0330

    time Sleep

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-
+import ( 
+     "fmt"
+     "time"
+)
 func main() {
 	fmt.Println("Hello World!")
 
@@ -18,6 +20,7 @@ func main() {
 	}
 	for {
 	fmt.Println("Infinite Loop ...")
+	time .Sleep(1 * time.Second)
 	}
 	fmt.Println(n)
 }
