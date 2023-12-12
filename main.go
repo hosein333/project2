8fe3a8239aaafbee235f67286d2bb881cf93c2d1
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-12 19:52:53 +0330

    add else

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-
 	x := 25
 
 	if x%5 ==0 && x != 25 {
 		fmt.Printf("%d is multiple of 5\n", x)
+	} else {
+                fmt.Printf("%d is not multiple of 5  or equal to 25\n", x)
 	}
+
 }
